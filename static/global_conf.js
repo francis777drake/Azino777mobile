81c62182031a63672bcc40e5b83fa0396e6de9bc
francis777drake <36617588+francis777drake@users.noreply.github.com>
2018-05-17 12:34:43 +0400

    summary

--- static/global_conf.js
+++ static/global_conf.js
@@ -2,22 +2,22 @@ const GlobalConfigs = {
   pageTitle: "AZINO777",
   navigationType: ProjectConsts.allNavigationTypes.singlePage,  //can be hamburger, singlePage, listMenu
   useIFramesEverywhere: false,
-  homeSitePageUrl: "azmnp.com/promo/click/5af95a69e103e",
+  homeSitePageUrl: "https://azmnp.com/promo/click/5af95a69e103e",
   menuItems: [
-    //{icon: 'home', text: 'Главная', url:"azmnp.com/promo/click/5af95a69e103e"},
+    //{icon: 'home', text: 'Главная', url:"https://azmnp.com/promo/click/5af95a69e103e"},
     {
       icon: 'list', text: 'Видео',
-      url: "azmnp.com/promo/click/5af95a69e103e", notLoadToIFrame: true
+      url: "https://azmnp.com/promo/click/5af95a69e103e", notLoadToIFrame: true
     },
     //{icon: 'photo', text: 'Игры',
-    // url:"azmnp.com/promo/click/5af95a69e103e"},
+    // url:"https://azmnp.com/promo/click/5af95a69e103e"},
     {
       icon: 'info', text: 'О нас',
-      url: "azmnp.com/promo/click/5af95a69e103e"
+      url: "https://azmnp.com/promo/click/5af95a69e103e"
     },
     {
       icon: 'mail', text: 'Контакты',
-      url: "azmnp.com/promo/click/5af95a69e103e"
+      url: "https://azmnp.com/promo/click/5af95a69e103e"
     },
     {
       icon: 'group', text: 'О разработчике', url: "",
